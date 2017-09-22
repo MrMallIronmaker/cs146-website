0051fb101e3f658154b15f99bd21f3ed6b22d901
Mark Miller <725mrm@gmail.com>
2017-09-22 15:49:41 -0700

    Added calendar and other things

---
+++ speaker_info.js
@@ -0,0 +1,51 @@
+var speakers = {
+	1 : {
+		name: "Stephen Lim",
+		title: "Product Design",
+		blurb: "We finally have a full quarter to work on a game at Stanford, "
+		 + "so we must make the most of that time and carefully choose what "
+		 + "we deliver.  We often get caught up with what we want to do, but "
+		 + "it is more important to think about what we offer.  What do you "
+		 + "want to stand out for?  Games are a subjective art, so it's not " 
+		 + "easy to convince players why your game will be especially fun, let "
+		 + "alone why they should choose to play.  Through various example "
+		 + "games, we will explore the experiences they offer, the motivations " 
+		 + "they satisfy and ultimately how well they execute on their promises.",
+		intro:
+		   "Stephen Lim is a lead producer at Riot Games.  He's worked in the "
+		 + "game industry for over 13 years as a producer of new games for Riot,"
+		 + " League of Legends, Project Titan at Blizzard Entertainment, Spore "
+		 + "at EA and as a programmer on weapons for Ratchet &amp; Clank at "
+		 + "Insomniac Games. Prior to games, he worked as a programmer at "
+		 + "enterprise startups in Silicon Valley. He is a defender of multiplayer"
+		 + " experiences and battles a never-ending tide of casual games."
+	},
+	2 : {
+		name: "Stone Librande",
+		title: "One-Page Designs",
+		blurb: 
+		   "In an effort to communicate more effectively and concisely, "
+		 + "Stone has been experimenting with a style of design documentation "
+		 + "called a \"one page design\". As the name implies, this is a document "
+		 + "that is exactly one page long. After all, why create a document with "
+		 + "more than one page if most people only read the first page anyway? "
+		 + "During this talk Stone will show numerous examples of one page "
+		 + "designs from Diablo 3, The Simpsons Game, Spore, and SimCity. He'll "
+		 + "discuss what works and what doesn't and explain how you can use "
+		 + "similar techniques to communicate key design ideas to your team.",
+				
+		intro:
+		   "Stone Librande, a Lead Designer at Riot Games, has worked in the "
+		 + "game industry for over 15 years on games such as SimCity and "
+		 + "Diablo 3. In addition to his full-time job designing video games, "
+		 + "he also teaches game design courses at Carnegie Mellon University’s "
+		 + "ETC program and runs design seminars around the world. An avid game "
+		 + "collector, Stone's walk-in closet holds more than 300 card and "
+		 + "board games, including 30 that he has designed himself."
+	}
+};
+
+var workshops = {
+	1 : "Unity Boot Camp led by a Riot Games engineer.",
+	2 : "Unity Boot Camp led by a Unity engineer"
+}
---
+++ tablemaker.js
@@ -0,0 +1,104 @@
+// make the table
+
+var table = $("#tablemaker");
+
+var dayToDayName = {
+	0 : "Sunday", 
+	1 : "Monday", 
+	2 : "Tuesday",
+	3 : "Wednesday",
+	4 : "Thursday",
+	5 : "Friday",
+	6 : "Saturday"
+};
+
+var monthToMonthName = {
+	8 : "September",
+	9 : "October",
+	10 : "November",
+	11 : "December"
+};
+
+function getDayOfWeek(inputDate, dayOfWeekIndex) {
+	var newDate = new Date(inputDate);
+	newDate.setDate(newDate.getDate() + dayOfWeekIndex);
+	return newDate;
+}
+
+function getWeek(week) {
+	var startDate = new Date(2017, 08, 24);
+	startDate.setDate(startDate.getDate() + 7 * (week - 1));
+	return startDate;
+}
+
+function dateEntryString(date) {
+	return dayToDayName[date.getDay()] + ', ' + monthToMonthName[date.getMonth()] + ' ' + date.getDate();
+}
+
+function speakerAtWeek(week) {
+	var speakerInfo = speakers[week];
+	var htmlSource;
+	if (speakerInfo) {
+		htmlSource = '<b>' + speakerInfo.name + ', ' + speakerInfo.title + '</b> '
+		  + speakerInfo.blurb + '<br><br>' + speakerInfo.intro;
+		
+	} else {
+		htmlSource = '<b>TBA, TBA</b> ';
+	}
+	return htmlSource;
+	
+}
+
+function workshopAtWeek(week) {
+	var description = workshops[week];
+	if (!workshops[week]) {
+		description = "TA-led working period.";
+	}
+	return description;
+}
+
+function homeworkAtWeek(week) {
+	if (week === 1) {
+		return 'HW 1 has a soft due date today.';
+	}
+	return 'HW ' + week + ' due.';
+}
+
+for (var week = 1; week <= 3; week++) {
+	var thisWeek = getWeek(week);
+	var nextWeek = getWeek(week + 1);
+
+	// get speaker
+	table.append('<tr><td>' 
+		+ dateEntryString(getDayOfWeek(thisWeek, 2)) 
+		+ '</td><td>'
+		+ speakerAtWeek(week)
+		+ '</td></tr>'
+	);
+
+	// get workshop
+	table.append('<tr><td>' 
+		+ dateEntryString(getDayOfWeek(thisWeek, 4)) 
+		+ '</td><td>'
+		+ workshopAtWeek(week)
+		+ '</td></tr>'
+	);
+
+	if (week != 10) {
+		table.append(
+			'<tr><td>' 
+			+ dateEntryString(getDayOfWeek(nextWeek, 1)) 
+			+ '</td><td>'
+			+ homeworkAtWeek(week)
+			+ '</td></tr>'
+		);
+	}
+
+	table.append(
+		'<tr><td>' 
+		+ 'more dates...' 
+		+ '</td><td>'
+		+ 'more information coming...'
+		+ '</td></tr>'
+	);
+}
